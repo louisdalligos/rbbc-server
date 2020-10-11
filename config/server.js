af9626d94dd6f56c5d4a92c88344cee3473eb50c
Louis Dalligos <zindex14@gmail.com>
2020-10-11 22:06:54 +0800

    Add mailgun email provider

--- config/server.js
+++ config/server.js
@@ -1,9 +1,16 @@
+const forgotPasswordTemplate = require("./email-templates/forgot-password");
+
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET"),
+    },
+    forgotPassword: {
+      from: "zindex14@gmail.com",
+      replyTo: "zindex14@gmail.com",
+      emailTemplate: forgotPasswordTemplate,
     },
   },
 });
