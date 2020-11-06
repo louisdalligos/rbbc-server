ffb26b34a7ae077a002a5c7f4af5bf850a8f220e
Louis Dalligos <zindex14@gmail.com>
2020-11-06 09:00:16 +0000

    Update server config

--- config/middleware.js
+++ config/middleware.js
@@ -1,7 +1,7 @@
 module.exports = {
   settings: {
     cors: {
-      origin: ['http://localhost:1337', 'https://rbbcrealty.com/', 'https://www.rbbcrealty.com'],
+      origin: ['http://localhost:1337', 'https://dashboard.rbbcrealty.com', 'https://rbbcrealty.com', 'https://www.rbbcrealty.com'],
     },
   },
 };
--- config/server.js
+++ config/server.js
@@ -3,6 +3,7 @@ const forgotPasswordTemplate = require("./email-templates/forgot-password");
 module.exports = ({ env }) => ({
   host: env("APP_HOST", "0.0.0.0"),
   port: env.int("NODE_PORT", 1337),
+url: 'https://dashboard.rbbcrealty.com',
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET"),
